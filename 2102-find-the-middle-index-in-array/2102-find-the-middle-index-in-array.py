0bfffd8a1065622811c249f0d9d0f69dd8a78459
dodi-adem <fikirmohammedadem@gmail.com>
2025-11-09 11:03:43 +0100

    Time: 0 ms (100%), Space: 17.8 MB (59.08%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
@@ -20,3 +20,14 @@ class Solution:
                 return i
 
         return -1
+
+        # this is the effcient way(space complexity = o(1)) i get from youtube
+
+        # total_sum = sum(nums)
+        # left_sum = 0
+        # for i,num in enumerate(nums):
+        #     if left_sum == total_sum - left_sum - num:
+        #         return i
+        #     left_sum += num
+        
+        # return -1
