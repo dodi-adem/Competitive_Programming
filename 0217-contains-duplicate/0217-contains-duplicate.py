f2d616886e6b8cc29c208e67b69671662a82eab4
dodi-adem <fikirmohammedadem@gmail.com>
2026-08-01 11:07:18 +0200

    Time: 11 ms (75.37%), Space: 32.3 MB (46.81%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        distinct = set()
+        for num in nums:
+            if num in distinct:
+                return True
+            distinct.add(num)
+        return False
+
+        
